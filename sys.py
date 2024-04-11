d8311a2ca1c7e7b2ca37f5f2b80d5d5b99f58778
Brunixs <112876996+Brunixs@users.noreply.github.com>
2024-04-11 18:28:24 +0000

    adicionado comentarios

--- sys.py
+++ sys.py
@@ -1,4 +1,4 @@
-from abc import ABC, classmethod, property
+from abc import ABC
 from datetime import datetime
 import textwrap
 
@@ -174,6 +174,7 @@ class Deposito(Transacao):
         if sucesso_transacao:
             conta.historico.add_transacoes(self)
 
+# Menu 
 def menu():
     menu = """\n
     ================ MENU ================
@@ -187,7 +188,7 @@ def menu():
     => """
     return input(textwrap.dedent(menu))
 
-
+# funçao de deposito
 def depositar(saldo, valor, extrato, /):
     if valor > 0:
         saldo += valor
@@ -198,7 +199,7 @@ def depositar(saldo, valor, extrato, /):
 
     return saldo, extrato
 
-
+# função de saque
 def sacar(*, saldo, valor, extrato, limite, numero_saques, limite_saques):
     excedeu_saldo = valor > saldo
     excedeu_limite = valor > limite
@@ -224,14 +225,14 @@ def sacar(*, saldo, valor, extrato, limite, numero_saques, limite_saques):
 
     return saldo, extrato
 
-
+# função para exibir o extrato
 def exibir_extrato(saldo, /, *, extrato):
     print("\n================ EXTRATO ================")
     print("Não foram realizadas movimentações." if not extrato else extrato)
     print(f"\nSaldo:\t\tR$ {saldo:.2f}")
     print("==========================================")
 
-
+# criação do usuario
 def criar_usuario(usuarios):
     cpf = input("Informe o CPF (somente número): ")
     usuario = filtrar_usuario(cpf, usuarios)
@@ -248,12 +249,12 @@ def criar_usuario(usuarios):
 
     print("=== Usuário criado com sucesso! ===")
 
-
+# filtrar usuarios 
 def filtrar_usuario(cpf, usuarios):
     usuarios_filtrados = [usuario for usuario in usuarios if usuario["cpf"] == cpf]
     return usuarios_filtrados[0] if usuarios_filtrados else None
 
-
+# criar conta do usuario
 def criar_conta(agencia, numero_conta, usuarios):
     cpf = input("Informe o CPF do usuário: ")
     usuario = filtrar_usuario(cpf, usuarios)
@@ -264,7 +265,7 @@ def criar_conta(agencia, numero_conta, usuarios):
 
     print("\n@@@ Usuário não encontrado, fluxo de criação de conta encerrado! @@@")
 
-
+# lista contas vinculadas a um usuario
 def listar_contas(contas):
     for conta in contas:
         linha = f"""\
@@ -275,7 +276,7 @@ def listar_contas(contas):
         print("=" * 100)
         print(textwrap.dedent(linha))
 
-
+# função principal
 def main():
     LIMITE_SAQUES = 3
     AGENCIA = "0001"
@@ -329,5 +330,5 @@ def main():
         else:
             print("Operação inválida, por favor selecione novamente a operação desejada.")
 
-
+# chamando a função principal
 main()
